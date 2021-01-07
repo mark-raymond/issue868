142049f1d3cf7353b7c2306ae2ad6cddab1a455c
Mark Raymond <mark.raymond@red-gate.com>
2021-01-07 17:32:04 +0000

    Trivial command line to execute SELECT @@SERVICENAME.

--- GetServiceName/Program.cs
+++ GetServiceName/Program.cs
@@ -1,12 +1,54 @@
 using System;
+using Microsoft.Data.SqlClient;
 
 namespace GetServiceName
 {
-    class Program
+    internal static class Program
     {
-        static void Main(string[] args)
+        private static int Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            if (args.Length != 3)
+            {
+                Console.Error.WriteLine("Usage: GetServiceName SERVER USERNAME PASSWORD");
+                return 1;
+            }
+
+            const string commandText = @"SELECT @@SERVICENAME";
+
+            var server = args[0];
+            var username = args[1];
+            var password = args[2];
+
+            var connectionString = new SqlConnectionStringBuilder
+            {
+                InitialCatalog = "master",
+                DataSource = server,
+                UserID = username,
+                Password = password,
+                IntegratedSecurity = false
+            };
+
+            try
+            {
+                Console.WriteLine($"Executing SQL: {commandText}");
+
+                using var connection = new SqlConnection(connectionString.ToString());
+                connection.Open();
+                using var command = new SqlCommand(commandText, connection);
+                var result = command.ExecuteScalar();
+                Console.WriteLine($"Result: {result}");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Caught an exception");
+                Console.WriteLine(e.Message);
+            }
+            finally
+            {
+                Console.WriteLine("End of program");
+            }
+
+            return 0;
         }
     }
 }
